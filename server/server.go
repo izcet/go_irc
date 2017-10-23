903b3abadd06e58bc9acc3876807b59e841ced9e
izcet <isaacjrhett@gmail.com>
2017-10-22 20:06:49 -0700

    it somewhat works. You can have named users, but no more than 2 [server.go]

--- server/server.go
+++ server/server.go
@@ -19,18 +19,29 @@ type	User struct {
 	conn		net.Conn
 }
 
+type	Message struct {
+	sender		User
+	reciever	[]User
+	text		string
+}
+
 var		activeUsers = make(map[int]User)
+var		messages = make(chan Message, 500)
 
 func	main() {
 	clientNum := 0
+	activeUsers[0] = User{0, "SERVERNAME", "PASSWORD", false, nil}
+
+	// initialize listener
 	listener, err := net.Listen("tcp", ":4444")
 	if (err != nil) {
 		panic(err)
 	}
-	server_dead = 0
-	fmt.Println("Server listening on", listener.Addr())
 	go serverCloser(listener)
+	fmt.Println("Server listening on", listener.Addr())
 
+	//begin main loop
+	server_dead = 0
 	for ; server_dead == 0; {
 		conn, err := listener.Accept()
 		if (err != nil) {
@@ -51,35 +62,35 @@ func	serverCloser(listener net.Listener) {
 	listener.Close()
 }
 
-
 func	handleClient(conn net.Conn, num int) { // work on a way to return a value, using channels
-	
+
 	fmt.Println("(client", num, "connected)")
 	defer fmt.Println("(client", num, "disconnected)")
 	defer conn.Close()
 
 	var buffer = make([]byte, NUM_BYTES)
-	
+
 	strlen, err := conn.Write([]byte("Welcome! What is your name?\n"))
 	strlen, err = conn.Read(buffer)
 	if (err != nil) {
 		fmt.Println("(client", num, "fucked up typing their own name)")
-		return 
+		return
 	}
 	name := string(buffer)[:strlen - 1]
-	
+
 	user := User{num, name, strconv.Itoa(num), true, conn} // password doesn't matter
 	// I literally just set this to cancel the compiler warning about unused strconv
 
 	activeUsers[num] = user
-	
-	go closeClient(user) // in case the server shuts down before the client is closed
-	
-	// insert code to add the client listener to other messages
+	go clientListen(user)
+	makeMessage(0, string(user.nickname + " has joined the server.\n"))
+
+	go deferClientClose(user) // in case the server shuts down before the client is closed
+		// insert code to add the client listener to other messages
 	// or a channel/broadcast system
 
 	for ; err == nil; {
-		
+
 		//bzero is hard to find
 		for x := 0; x < NUM_BYTES; x++ {
 			buffer[x] = 0
@@ -95,12 +106,10 @@ func	handleClient(conn net.Conn, num int) { // work on a way to return a value, 
 			str = str[:strlen - 1]
 			if (strings.Compare(str, "stop") == 0) {
 				server_dead = num
-				fmt.Println("Client", num, "shut down the server.")
-				conn.Write([]byte("You have shut down the server.\n"))
+				makeMessage(0, string(user.nickname + " shut down the server.\n"))
 				break
 			} else {
-				fmt.Println("Client number", num, "says", str)
-				strlen, err = conn.Write(buffer)
+				makeMessage(num, str)
 			}
 		}
 	}
@@ -108,16 +117,51 @@ func	handleClient(conn net.Conn, num int) { // work on a way to return a value, 
 	if (err != nil) {
 		fmt.Println(err)
 	}
+	user.active = false
+	makeMessage(0, string(user.nickname + " has disconnected.\n"))
 }
 
-func	closeClient(user User) {
+func	clientListen(user User) {
+	for ; user.active; {
+		msg := <-messages
+		if (user.active && (msg.sender.IDNumber != user.IDNumber)) {
+			var str string
+			if (msg.sender.IDNumber == 0) {
+				str = msg.text + "\n"
+			} else {
+				str = "[" + msg.sender.nickname + "] " + msg.text + "\n"
+			}
+			_, err := user.conn.Write([]byte(str))
+			if (err != nil) {
+				fmt.Println(err)
+			}
+		}
+	}
+}
+
+func	deferClientClose(user User) {
 	for ; ((server_dead == 0) && user.active); {
 		continue
 	}
 	if (user.active) {
 		user.conn.Write([]byte("The server has been shut down.\n"))
+		user.active = false
 	}
 }
+
+func	makeMessage(user int, text string) {
+	fmt.Println("[", user, "]", text)
+	msg := Message{activeUsers[user], nil, text}
+	messages <- msg
+}
+
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////////
+// here be dragons
 
 type	myError struct {
 	what string
