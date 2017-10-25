f462f612e753ff8b368dda3ee3df3bc06c827173
brett merrill <brett.j.merrill94@gmail.com>
2017-10-25 11:12:55 -0700

    fixed the 3 user problem and made a lot of comments

--- server/client.go
+++ server/client.go
@@ -3,7 +3,7 @@ package main
 import (
 	"net"
 	"fmt"
-	"strings"
+//	"strings"
 //	"strconv"
 )
 
@@ -25,10 +25,10 @@ type	Message struct {
 func	handleClient(conn net.Conn, num int) {
 
 	fmt.Println("(client", num, "connected)")
-	defer fmt.Println("(client", num, "disconnected)")
-	defer conn.Close()
+	defer fmt.Println("(client", num, "disconnected)")//nice logging messages
+	defer conn.Close()//nice cleanup
 
-	var buffer = make([]byte, NUM_BYTES)
+	var buffer = make([]byte, NUM_BYTES)//
 
 	strlen, err := conn.Write([]byte("Welcome! What is your name?\n"))
 	strlen, err = conn.Read(buffer)
@@ -38,18 +38,18 @@ func	handleClient(conn net.Conn, num int) {
 	}
 	name := string(buffer)[:strlen - 1]
 
-	user := User{num, name, "", true, conn, make(chan *Message, 512)} // password doesn't matter
+	user := User{num, name, "", true, conn, make(chan *Message, 512)} //set up a user with a name and a pointer to a message struct channel
 
-	activeUsers[num] = user
+	activeUsers[num] = user//keep track of users in global
 	go clientListen(user)
 	makeMessage(0, string(user.nickname + " has joined the server"))
-	go deferClientClose(user) // in case the server shuts down before the client is closed
+	defer ClientClose(user) // in case the server shuts down before the client is closed
 
 	for ; err == nil; {
 
 		//bzero is hard to find
-		for x := 0; x < NUM_BYTES; x++ {
-			buffer[x] = 0
+		for i, _ := range buffer {
+			buffer[i] = 0
 		}
 
 		//take the message from the client
@@ -60,8 +60,8 @@ func	handleClient(conn net.Conn, num int) {
 		if (strlen != 0) {
 			str := string(buffer)
 			str = str[:strlen - 1]
-			if (strings.Compare(str, "stop") == 0) {
-				server_dead = num
+			if (str == "stop") {
+				server_dead = 1
 				makeMessage(0, string(user.nickname + " shut down the server"))
 				break
 			} else {
@@ -76,23 +76,24 @@ func	handleClient(conn net.Conn, num int) {
 
 func	clientListen(user User) {
 	for ; user.active; {
-		msg := <-user.inbox
-		if (user.active) {
-			var str string
-			if (msg.sender.IDNumber == 0) {
-				str = "<" + msg.text + ">\n"
-			} else {
+		select {
+		case msg := <-user.inbox:
+			if (user.active) {
+				var str string
+				if (msg.sender.IDNumber == 0) {
+					str = "<" + msg.text + ">\n"
+				} else {
 				str = "[" + msg.sender.nickname + "] " + msg.text + "\n"
-			}
-			user.conn.Write([]byte(str))
+				}
+				user.conn.Write([]byte(str))
+			}	
+		default:
+			continue
 		}
 	}
 }
 
-func	deferClientClose(user User) {
-	for ; ((server_dead == 0) && user.active); {
-		continue
-	}
+func	ClientClose(user User) {
 	if (user.active) {
 		user.conn.Write([]byte("The server has been shut down.\n"))
 		user.active = false
@@ -100,9 +101,9 @@ func	deferClientClose(user User) {
 }
 
 func	makeMessage(user int, text string) {
-	msg := Message{activeUsers[user], nil, text}
+	msg := &Message{activeUsers[user], nil, text}
 	select {
-	case serverMessages <-&msg:
+	case serverMessages <-msg:
 		fmt.Println("Sent message to server")
 	default:
 		fmt.Println("No message sent")
--- server/server.go
+++ server/server.go
@@ -6,30 +6,30 @@ import (
 	//	"strings"
 	//	"strconv"
 )
-
-const	NUM_BYTES = 512
+//Global state bad
+const	NUM_BYTES = 512 //maximum size of message
 
 var		server_dead int
 
-var		activeUsers = make(map[int]User)
-var		serverMessages = make(chan *Message, 500)
+var		activeUsers = make(map[int]User) //global map for something with users???
+var		serverMessages = make(chan *Message, 500) //global buffered *Message channel ???
 
 func	main() {
-	clientNum := 0
-	activeUsers[0] = User{0, "SERVERNAME", "PASSWORD", false, nil, make(chan *Message, 512)}
+	clientNum := 0 //why keep track of total clients
+	activeUsers[0] = User{0, "SERVERNAME", "PASSWORD", false, nil, make(chan *Message, 512)} //the first member of the user map is the server possibly for server connection
 
 	// initialize listener
-	listener, err := net.Listen("tcp", ":4444")
-	if (err != nil) {
+	listener, err := net.Listen("tcp", ":4444") //how are you gonna have a server without a listener 10/10
+	if (err != nil) { //maybe the port is taked already
 		panic(err)
 	}
-	go serverCloser(listener)
-	fmt.Println("Server listening on", listener.Addr())
+	defer listener.Close()//close the listener after it's use
+	fmt.Println("Server listening on", listener.Addr())//helps to know the address to tell other people
 
-	go serverDistributeMessages()
+	go serverDistributeMessages() //the messages need to find their way around the network
 
 	//begin main loop
-	server_dead = 0
+	server_dead = 0//why ?????
 	for ; server_dead == 0; {
 		conn, err := listener.Accept()
 		if (err != nil) {
@@ -37,32 +37,25 @@ func	main() {
 			continue
 		}
 		clientNum += 1
-		go handleClient(conn, clientNum)
+		go handleClient(conn, clientNum)//need concurrent routines for each client
 	}
 
 	fmt.Println("Server no longer listening. Shutting down.")
 }
 
-func	serverCloser(listener net.Listener) {
-	for ; server_dead == 0 ; {
-		continue
-	}
-	listener.Close()
-}
-
 func	serverDistributeMessages() {
-	for ; server_dead == 0 ; {
+	for ; server_dead == 0 ; {//main loop again
 		select {
-		case msg := <-serverMessages:
+		case msg := <-serverMessages: //if a message appears it has to find it's way to everyone it applies to
 			fmt.Println("Server recieved message")
 			for num, user := range activeUsers {
 				//newMsg := *msg
-				fmt.Println(num)
-				if (msg.sender.IDNumber != user.IDNumber) && (user.IDNumber != 0) && (user.active) {
+				fmt.Println(num)//
+				if (msg.sender.IDNumber != user.IDNumber) && (user.IDNumber != 0) && (user.active) {// user cant send themselves a message otherwise put it in everyones mailbox
 					user.inbox <-msg //newMsg
 				}
 			}
-		default:
+		default://the serverMessages isn't always busy
 			continue
 
 		}
