3488d4b59d437a3dbe09b920b14ffaa90092a528
Stephen Gardner <stephenbgardner@gmail.com>
2017-10-29 07:42:49 -0700

    Parse messages; fix handling for messages that exceed maximum length of 512

--- server/client.go
+++ server/client.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net"
 	"fmt"
+	"strings"
 )
 
 func	newClient(conn net.Conn, serv *Server) (*Client, error) {
@@ -25,18 +26,25 @@ func	newClient(conn net.Conn, serv *Server) (*Client, error) {
 
 func	setClientInbound(client *Client) {
 	err := error(nil)
-	buffer := make([]byte, 512)
+	buffer := make([]byte, 513)
 	var strlen int
 	for ; err == nil; {
 		strlen, err = client.connection.Read(buffer)
 		if (strlen <= 1) {
 			err = sendMessageAlongConnection("Error: Message length was too short.\n", client.connection)
 		} else if (strlen > 512) {
+			for {
+				// Throw out excess bytes
+				strlen, err = client.connection.Read(buffer)
+				if (strings.Index(string(buffer)[0:strlen], "\n") != -1) {
+					break
+				}
+			}
 			err = sendMessageAlongConnection("ERROR: Message length was too long.\n", client.connection)
 		} else if (err != nil) {
 			fmt.Println(err)
 		} else {
-			handleClientInput(client, string(buffer)[0:len(buffer) - 1], strlen - 1)
+			handleClientInput(client, string(buffer)[0:strlen], strlen)
 		}
 	}
 }
@@ -54,13 +62,60 @@ func	setClientOutbound(client *Client) {
 }
 
 func	handleClientInput(client *Client, input string, strlen int) {
+	var prefix, cmd string
+	var params []string
 	err := error(nil)
+/*
 	if (input[0] == '/') {
 		err = callCommand(client, input, strlen)
 	} else {
 		msg := makeMessage(client, input)
 		client.Incoming <-msg
 	}
+*/
+	/**  OPTIONAL PREFIX **/
+	input = strings.TrimSuffix(input, "\r\n")
+	if (input[0] == ':') {
+		fields := strings.SplitN(input, " ", 2)
+		if (len(fields) < 2) {
+			// Incomplete input
+			return
+		}
+		prefix = fields[0][1:]
+		input = strings.TrimLeft(fields[1], " ")
+	}
+	if (len(input) < 1) {
+		// (Remaining) input is empty
+		return
+	}
+	/** COMMAND **/
+	fields := strings.SplitN(input, " ", 2)
+	if (len(fields[0]) < 1) {
+		// Missing or incorrectly spaced command
+		return
+	}
+	// Commands should be uppercase anyway, but just in case...
+	cmd = strings.ToUpper(fields[0])
+	/** OPTIONAL PARAMETERS **/
+	if (len(fields) > 1) {
+		input = strings.TrimLeft(fields[1], " ")
+		for {
+			if (input[0] == ':') {
+				// Message
+				params = append(params, input[1:])
+				break
+			}
+			// Command parameters
+			fields := strings.SplitN(input, " ", 2)
+			params = append(params, fields[0])
+			if (len(fields) > 1) {
+				input = strings.TrimLeft(fields[1], " ")
+			} else {
+				break
+			}
+		}
+	}
+	fmt.Printf("Prefix: %s Cmd: %s Params: %q\n", prefix, cmd, params)
 	if (err != nil) {
 		fmt.Println(err)
 	}
